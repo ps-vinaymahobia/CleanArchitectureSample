c122ac265f9dfde83d7ee9c337659a84f5dc503d
Vinay Mahobia <vinay.mahobia@publicissapient.com>
2024-01-03 08:59:02 +0530

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    includeBuild("gradle-dependencies") //module contains build logic
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CleanArchitectureSample"
+include(":app")
+include(":ui")
+include(":domain")
