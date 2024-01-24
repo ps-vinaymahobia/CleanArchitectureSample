606b4d6aed45cf5dbe73085d360b1ddeb8bc97b4
Vinay Mahobia <vinay.mahobia@publicissapient.com>
2024-01-24 15:36:44 +0530

    updates FakeDataProvider for UTs

--- presentation/src/test/kotlin/utils/FakeDataProvider.kt
+++ presentation/src/test/kotlin/utils/FakeDataProvider.kt
@@ -65,24 +65,14 @@ object FakeDataProvider {
 
 
     val fakePresentationProductListItem1 = ProductListItemPresentationModel(
-        brand = "Apple",
         id = 1,
         title = "iPhone 15",
-        category = "",
-        price = 200,
-        stock = 1,
-        rating = 2.2,
         thumbnail = ""
     )
 
     val fakePresentationProductListItem2 = ProductListItemPresentationModel(
-        brand = "Samsung",
         id = 2,
         title = "S23",
-        category = "",
-        price = 400,
-        stock = 1,
-        rating = 2.2,
         thumbnail = ""
     )
 
@@ -90,17 +80,10 @@ object FakeDataProvider {
     val fakePresentationProductList = ProductListPresentationModel(listOf(fakePresentationProductListItem1, fakePresentationProductListItem2))
 
     val fakePresentationProductDetails1 = ProductDetailsPresentationModel(
-        brand = "Samsung",
         id = 1,
         title = "S23",
-        category = "",
         description = "",
-        price = 400,
-        stock = 2,
-        discountPercentage = 2.2,
         images = emptyList(),
-        rating = 2.2,
-        thumbnail = ""
     )
 
 }
